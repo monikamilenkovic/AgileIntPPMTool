b1c036ffea4cd725d91c2d32ae18e6147ed61594
Monika <monikamilenkovic15@gmail.com>
2020-08-24 10:24:27 +0200

    Added MapValidationServiceError

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -2,15 +2,23 @@ package io.agileintelligence.ppmtool.web;
 
 import io.agileintelligence.ppmtool.domain.Project;
 import io.agileintelligence.ppmtool.repository.ProjectRepository;
+import io.agileintelligence.ppmtool.services.MapValidationErrorService;
 import io.agileintelligence.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/project")
@@ -19,9 +27,26 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
-    @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
-        Project project1 = projectService.saveOrUpdateProject(project);
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+    @PostMapping("") //ako stoji samo @Valid bez binding result, onda kad uradimo post vraca gresku 400 i vraca objekat sa svim greskama validacije koje su se desile nad objektom
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){ //trazimo validirani body response //Binding resault je interfejs koji zove validator na object //analizira objekat i gleda da li ima gresaka u kreiranju objekta
+//        if(result.hasErrors()){ //da vidimo da li je proslo validacije ili ima gresaka
+//            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST); // promenimo da je povratna vrednost Generic(?) da bismo ovde vratili string i rekli da je u pitanju los http zahtev
+//        } //bitno nam je da hvatamo izuzetke sa smislom da bismo posle pomocu react-a handlovali lakse i uzeli sa servera i prebacili na front
+//         //hocemo da nam odgovor vrati ovaj format "field":'error message'..
+//        if(result.hasErrors()){
+//            Map<String, String> errorMap = new HashMap<>();
+//            for (FieldError error: result.getFieldErrors()){
+//                errorMap.put(error.getField(), error.getDefaultMessage());
+//            }
+//            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+//        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null){
+            return errorMap;
+        }
+        Project project1 = projectService.saveOrUpdateProject(project); //ovde se desi greska kad nije unique ID, a ne za validaciju, validacija prodje jer ona samo gleda taj objekat a nema dodira sa bazom
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
 
